94fa0fd73eab7373f5b788ef011aaf78219f2bea
Deepak SP <94455864+deepak-sp-inapp@users.noreply.github.com>
2022-10-07 21:07:53 +0530

    Restricted to use only developement js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
-	module.exports = require('./dist/lightweight-charts.esm.production.js');
-} else {
-	module.exports = require('./dist/lightweight-charts.esm.development.js');
-}
+module.exports = require('./dist/lightweight-charts.esm.development.js');
